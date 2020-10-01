27f0025df70ca5be73285162eeebf40abde8485f
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-10-01 18:25:40 +0200

    init

---
+++ pages/register/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <NuxtLogo />
+    Register Content
+    {{ count }}
+    <br/>
+    {{ valt }}
+    <button @click="inc">asdsd</button>
+    <button @click="inc2">asdsd</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        valt:0
+      }
+    },
+    fetch(){
+
+    },
+    async asyncData(){
+    }
+    ,
+    methods:{
+      inc(){
+        this.$store.commit('increment')
+      },
+      inc2(){
+        this.valt++
+      }
+
+    },
+    computed:{
+      count(){
+        return this.$store.state.counter
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
