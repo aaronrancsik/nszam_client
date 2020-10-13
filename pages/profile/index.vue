1f49455e73615a8feafa6aabe2cf998862d2ef75
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-10-13 20:55:23 +0200

    fix ui

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,50 +1,52 @@
 <template>
-  <div class="container">
-  <h1>Profile</h1>
-  <div v-if="$auth.loggedIn">
-    <v-card-text>{{$auth.user.name}}</v-card-text>
-      <div class="row">
-        <v-card-text>{{ profile.sumPosts }} posts</v-card-text>
-        <v-card-text>{{ profile.followers }} followers</v-card-text>
-      </div>
-    <div>
-      <v-btn @click="logoutFB">LOGOUT</v-btn>
-    </div>
+  <div>
+    <v-row>
+      <v-col>
+        <h1>{{ $auth.user.name }}</h1>
+        <h3>{{ profile.posts.length }} posts - 4 followers</h3>
+      </v-col>
+      <v-col> </v-col>
+      <v-col>
+        <div>
+          <v-btn @click="logoutFB">LOGOUT</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+    <h4>{{ profile.bio }}</h4>
     <div>
       <post-list-view :posts="profile.posts"></post-list-view>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-  async asyncData({$axios}) {
-    const { data } = await $axios.get(`/profile`)
-    return { profile: data }
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get(`/profile`);
+    return { profile: data };
   },
   methods: {
-    async logoutFB(){
-      await this.$auth.logout('facebook')
-    }
-  }
-}
+    async logoutFB() {
+      await this.$auth.logout("facebook");
+    },
+  },
+};
 </script>
 
 <style scoped>
-.container {
-  /*margin: 0 auto;*/
-  /*min-height: 100vh;*/
-  /*display: flex;*/
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.userInfo {
-  /*flex-direction: column;*/
-}
-.row {
-  display: flex;
-  flex-direction: row;
-}
+/*.container {*/
+/*  !*margin: 0 auto;*!*/
+/*  !*min-height: 100vh;*!*/
+/*  !*display: flex;*!*/
+/*  justify-content: center;*/
+/*  align-items: center;*/
+/*  text-align: center;*/
+/*}*/
+/*.userInfo {*/
+/*  !*flex-direction: column;*!*/
+/*}*/
+/*.row {*/
+/*  display: flex;*/
+/*  flex-direction: row;*/
+/*}*/
 </style>
