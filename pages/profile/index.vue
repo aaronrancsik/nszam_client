343d08713b6252ff81862d779bcf0fce09ae6592
Gabriella Rozsa <gabriella.rozsa@yahoo.com>
2020-10-13 20:17:06 +0200

    profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>
-    Profile
+  <div class="container">
+  <h1>Profile</h1>
+  <div v-if="$auth.loggedIn">
+    <v-card-text>{{$auth.user.name}}</v-card-text>
+      <div class="row">
+        <v-card-text>{{ profile.sumPosts }} posts</v-card-text>
+        <v-card-text>{{ profile.followers }} followers</v-card-text>
+      </div>
+    <div>
+      <v-btn @click="logoutFB">LOGOUT</v-btn>
+    </div>
+    <div>
+      <post-list-view :posts="profile"></post-list-view>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  async asyncData({$axios}) {
+    const { data } = await $axios.get(`/profile`)
+    return { profile: data }
+  },
+  methods: {
+    async logoutFB(){
+      await this.$auth.logout('facebook')
+    }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.container {
+  /*margin: 0 auto;*/
+  /*min-height: 100vh;*/
+  /*display: flex;*/
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.userInfo {
+  /*flex-direction: column;*/
+}
+.row {
+  display: flex;
+  flex-direction: row;
+}
 </style>
