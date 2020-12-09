77cc81db1df1036b5f8157db096a35db2a09f811
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-12-09 01:46:52 +0100

    fix

--- pages/friends/index.vue
+++ pages/friends/index.vue
@@ -5,6 +5,20 @@
 <!--      <Request/>-->
 <!--    </div>-->
     <h1>Friends</h1>
+    <div>
+      <v-row
+        v-for="f in JSON.parse($route.query.profile)['friends']"
+      >
+        <v-col class="pa-2">
+         <v-card style="ma-5" class="pa-2">
+            <v-card-text>   {{ f }} </v-card-text>
+           <v-card-actions>
+             <v-btn>Send request to {{f.name}}  </v-btn>
+           </v-card-actions>
+         </v-card>
+        </v-col>
+      </v-row>
+    </div>
     <v-spacer></v-spacer>
     <v-list v-for="friend in friends"
             color="rgba(0,0,0,0.2)"
